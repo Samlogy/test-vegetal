b98961c03415f198eb3b9451f16b6b0ca5c2d592
samy senani <aceportgasd235@gmail.com>
2023-03-22 10:26:54 +0100

    refactoring: artciles page

--- components/Articles.jsx
+++ components/Articles.jsx
@@ -1,30 +1,23 @@
 import { useContext } from "react";
-// import { AuthContext } from "../store/AuthProvider";
-// import { PopupContext } from "../store/PopupProvider";
 import { GlobalContext } from "../store/GlobalStore";
 
 export default function Articles() {
-  // const { auth } = useContext(AuthContext);
-  // const actions = useContext(PopupContext);
   const { articles } = useContext(GlobalContext);
 
   // console.log('articles: ', articles)
-
-  const { profile } = useContext(GlobalContext);
-  if (articles.error)
-    return (
-      <div className="flex flex-col w-full md:w-2/3 lg:w-3/4 mb-4">
-        <h2 className="font-bold text-xl mb-4"> Articles</h2>
-        <p className="bg-gray-100 shadow-md p-4 rounded-md w-full text-red-600">
-          {articles?.error?.message}
-        </p>
-      </div>
-    );
   return (
     <div className="flex flex-col w-full md:w-2/3 lg:w-3/4 mb-4">
       <h2 className="font-bold text-xl mb-4"> Articles</h2>
 
-      {articles.data && Array.isArray(articles.data) && [].length > 0 ? (
+      {articles.isFetching ? (
+        <p className="w-ful">Loading ...</p>
+      ) : articles.error ? (
+        <p className="bg-gray-100 shadow-md p-4 rounded-md w-full text-red-600">
+          {articles?.error?.message}
+        </p>
+      ) : articles.data &&
+        Array.isArray(articles.data) &&
+        articles.data.length > 0 ? (
         <table className="overflow-x-auto shadow-md rounded-lg w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -44,8 +37,6 @@ export default function Articles() {
           </thead>
 
           <tbody className="w-full">
-            {/* ( */}
-
             {articles.data.map((article) => (
               <tr
                 key={article.id}
@@ -63,16 +54,12 @@ export default function Articles() {
                 </td>
               </tr>
             ))}
-            {/* ) : (
-            <tr className="bg-white dark:bg-gray-800 dark:border-gray-700">
-              
-              
-            </tr>
-          )} */}
           </tbody>
         </table>
       ) : (
-        <p className="bg-gray-100 shadow-md p-4 rounded-md w-full"> Aucun article ! </p>
+        <p className="bg-gray-100 shadow-md p-4 rounded-md w-full">
+          Aucun article !{" "}
+        </p>
       )}
     </div>
   );
--- store/GlobalStore.jsx
+++ store/GlobalStore.jsx
@@ -3,8 +3,8 @@ import { createContext, useState } from 'react';
 export const GlobalContext = createContext(null);
 
 export default function GlobalProvider ({children})  {
-    const [profile, setProfile] = useState({fetch:false, data: {}, error: null})
-    const [articles, setArticles] = useState({fetch:false, data: [], error: null})
+    const [profile, setProfile] = useState({fetch:false, data: {}, error: null, isFetching: true})
+    const [articles, setArticles] = useState({fetch:false, data: [], error: null, isFetching: true})
     
     return (
         <GlobalContext.Provider value={{ articles, setArticles, profile, setProfile }}>
